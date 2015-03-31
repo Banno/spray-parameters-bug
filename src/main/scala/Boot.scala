31a51481b490349f990046d95746c214ddc6506b
Adam Shannon <adam.shannon@banno.com>
2015-03-31 13:00:35 -0500

    try and be more explicit about the resulting type to try and get implicit resolution to help us.

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,5 +1,6 @@
 package com.banno.spray
 import spray.routing.{Directive, Directives}
+import spray.routing.directives._
 import shapeless.{::, HNil}
 
 object Boot extends Directives {
@@ -17,6 +18,6 @@ object Boot extends Directives {
     parameters('thing.as[Boolean], 'thing2.as[Boolean])
 
   // fails compile
-  def multiParams: Directive[Boolean :: Boolean :: HNil] =
+  def multiParams2: Directive[Boolean :: Boolean :: HNil] =
     parameters('thing.as[Boolean] ? true, 'thing2.as[Boolean] ? false)
 }
